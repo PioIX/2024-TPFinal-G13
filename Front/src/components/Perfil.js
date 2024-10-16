5fde78b617b5a65347841bc40c86d07d3725bcdd
santisalerno <ssalerno@pioix.edu.ar>
2024-10-16 10:12:04 -0300

    santi
    ..

---
+++ Front/src/components/Perfil.js
@@ -0,0 +1,10 @@
+"use client"
+export default function Perfil ( nombre, imgSrc, Text, descripcion ){
+    return (
+        <div className="perfil">
+            <img src={imgSrc} alt={Text} />
+            <h3>{nombre}</h3>
+            <p>{descripcion}</p>
+        </div>
+    );
+};
