a17022395e062c00c4437b3f1a7111c020b6f439
santisalerno <ssalerno@pioix.edu.ar>
2024-11-19 17:04:55 +0100

    ..

--- Front/src/app/home/propiedades/page.js
+++ Front/src/app/home/propiedades/page.js
@@ -7,9 +7,6 @@ import { useRouter } from "next/navigation";
 import Registro from "../../../components/Registro";
 import Login from "../../../components/Login";
 import Button from "../../../components/Button";
-import Text from "../../../components/Text";
-import ButtonMensaje from "../../../components/ButtonMensaje";
-import Chat from "../../../components/Chat";
 import Propiedad from "../../../components/Propiedad";
 
 export default function propiedades() {
