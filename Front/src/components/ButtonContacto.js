b3dd1af73f1c8d080ebd2c52175e1c0fb292a415
tomasmaraval <tmaraval@pioix.edu.ar>
2024-11-22 01:07:40 -0300

    .

---
+++ Front/src/components/ButtonContacto.js
@@ -0,0 +1,42 @@
+"use client"
+import clsx from 'clsx';
+import styles from './ButtonContacto.module.css';
+import { useEffect } from "react";
+
+/*export default function Button (props){
+    return(
+        <button onClick={props.onClick}>{props.text}</button>
+    )
+}*/
+
+
+export default function ButtonContacto({ onClick, text, variant }) {
+    useEffect(() => {
+      // Función para manejar la tecla Enter
+      const handleKeyPress = (event) => {
+        if (event.key === "Enter") {
+          onClick();  // Ejecutar la función onClick cuando se presiona Enter
+        }
+      };
+  
+      // Agregar el listener para el evento keydown
+      document.addEventListener("keydown", handleKeyPress);
+  
+      // Limpiar el listener cuando el componente se desmonte
+      return () => {
+        document.removeEventListener("keydown", handleKeyPress);
+      };
+    }, [onClick]);  // Solo se vuelve a crear el efecto si cambia onClick
+  
+    return (
+      <button
+        className={clsx({
+          [styles.button]: true,
+          [styles[variant]]: variant,  // Usar el prop variant si se pasa uno
+        })}
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    );
+  }
