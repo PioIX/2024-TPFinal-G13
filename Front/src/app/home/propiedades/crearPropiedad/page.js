69d4affbffae087f5b7673bdb6cd9af4d24e51e2
lucashelueni <163159971+lucashelueni@users.noreply.github.com>
2024-11-22 03:23:27 -0300

    terminado todo todo todo

--- Front/src/app/home/propiedades/crearPropiedad/page.js
+++ Front/src/app/home/propiedades/crearPropiedad/page.js
@@ -1,7 +1,7 @@
 "use client"
 import { useEffect, useState } from "react"
 import Image from "next/image";
-import styles from "../../../page.module.css";
+import styles from "../page.module.css";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import AddPropiedad from "../../../../components/AddPropiedad";
